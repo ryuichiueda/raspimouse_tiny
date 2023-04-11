04ab0f87ce28908e8480b026932208faa3257acd
Ryuichi Ueda <ryuichiueda@gmail.com>
2023-04-11 05:44:10 +0000

    Add direct frequent input

--- raspimouse_tiny/motors.py
+++ raspimouse_tiny/motors.py
@@ -4,6 +4,8 @@ import rclpy
 from rclpy.node import Node
 from raspimouse_tiny_msgs.srv import PutMotorFreqs
 from raspimouse_tiny_msgs.srv import SwitchMotors
+from raspimouse_tiny_msgs.msg import MotorFreqs
+#from geometry_msgs.msg import Twist
 
 class Motors():
     def __init__(self, node_ref):
@@ -11,7 +13,7 @@ class Motors():
         #self.pub = self.node.create_publisher(LightSensorValues, "motors", 10)
         #self.node.create_timer(0.1, self.cb)
 
-        #self.sub_freqs = rospy.Subscriber('motor_raw', MotorFreqs, self.callback_motor_raw)
+        self.sub_freqs = self.node.create_subscription(MotorFreqs, 'motor_raw', self.callback_motor_raw, 10)
         #self.sub_cmd_vel = rospy.Subscriber('cmd_vel', Twist, self.callback_cmd_vel)
         self.srv_motor_power = self.node.create_service(SwitchMotors, 'switch_motors', self.callback_motor_sw)
         self.srv_freqs = self.node.create_service(PutMotorFreqs, 'put_motor_freqs', self.callback_put_freqs)
@@ -36,10 +38,27 @@ class Motors():
                 f.write("%s %s %s\n" % (request.left, request.right, request.duration))
             response.accepted = True
         except:
-            rospy.logerr("cannot write to " + devfile)
+            self.node.get_logger().info("cannot write to " + devfile)
             response.accepted = False
 
         return response    
+
+
+    def callback_motor_raw(self, msg):
+        lfile = '/dev/rtmotor_raw_l0'
+        rfile = '/dev/rtmotor_raw_r0'
+        #self.node.get_logger().info("cannot write to rtmotor_raw_*: %s" % msg.left)
+        
+        try:
+            lf = open(lfile,'w')
+            rf = open(rfile,'w')
+            lf.write("%s\n" % msg.left)
+            rf.write("%s\n" % msg.right)
+        except:
+            self.node.get_logger().info("cannot write to rtmotor_raw_*")
+    
+        lf.close()
+        rf.close()
 
 
 def main():
